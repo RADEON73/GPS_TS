ed67f2af39804a0c03d3bbf7fb854fa783b01760
Радик Алиякберов <radeon73@rambler.ru>
2025-05-25 16:46:02 +0400

    Тестовые

--- GPS_TS_Server/Application.cpp
+++ GPS_TS_Server/Application.cpp
@@ -6,6 +6,7 @@
 #include <qcoreapplication.h>
 #include <qhostaddress.h>
 #include <qstring.h>
+#include <qlocalsocket.h>
 
 Application::Application(int& argc, char** argv) : QCoreApplication(argc, argv)
 {
@@ -14,6 +15,11 @@ Application::Application(int& argc, char** argv) : QCoreApplication(argc, argv)
 
     auto& logToFile = Settings::instance().logging().logToFile;
     Logger::instance().init("", true, logToFile);
+
+    if (!visualizerServer.listen("GPS_TS_Visualizer"))
+        Logger::instance().error("Не удалось запустить сервер для визуализатора!");
+    else
+        connect(&visualizerServer, &QLocalServer::newConnection, this, &Application::onNewConnection);
 
     auto& comPort = Settings::instance().serial().port;
     serial_port.openPort(comPort);
@@ -25,9 +31,33 @@ Application::Application(int& argc, char** argv) : QCoreApplication(argc, argv)
 
     connect(&serial_port, &SerialPort::dataTimeout, &server, &TCPServer::setDataNotActualState);
     connect(&serial_port, &SerialPort::dataRestored, &server, &TCPServer::setDataActualState);
+
+    connect(&Logger::instance(), &Logger::logMessage, this, &Application::sendToVisualizer);
 }
 
 Application::~Application()
 {
+    visualizerServer.close();
     serial_port.closePort(); 
+}
+
+void Application::onNewConnection()
+{
+    QLocalSocket* client = visualizerServer.nextPendingConnection();
+    visualizerClients.append(client);
+
+    connect(client, &QLocalSocket::disconnected, [this, client]() {
+        visualizerClients.removeOne(client);
+        client->deleteLater();
+        });
+}
+
+void Application::sendToVisualizer(const QString& message) const
+{
+    for (QLocalSocket* client : visualizerClients) {
+        if (client->state() == QLocalSocket::ConnectedState) {
+            client->write(message.toUtf8());
+            client->flush();
+        }
+    }
 }
--- GPS_TS_Server/Application.h
+++ GPS_TS_Server/Application.h
@@ -3,6 +3,7 @@
 #include "TcpServer.h"
 #include "TimeSynchronizer.h"
 #include <qcoreapplication.h>
+#include <qlocalserver.h>
 
 class Application : public QCoreApplication
 {
@@ -12,8 +13,14 @@ public:
     Application(int& argc, char** argv);
     ~Application() final;
 
+private slots:
+    void onNewConnection();  // Обработчик новых подключений визуализатора
+    void sendToVisualizer(const QString& message) const;  // Отправка данных в визуализатор
+
 private:
     TimeSynchronizer synchronizer;
     SerialPort serial_port{ &synchronizer };
     TCPServer server{ &synchronizer };
+    QLocalServer visualizerServer;  // Сервер для визуализатора
+    QList<QLocalSocket*> visualizerClients;  // Список подключённых визуализаторов
 };
